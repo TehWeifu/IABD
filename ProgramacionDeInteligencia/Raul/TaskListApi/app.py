090101f268e2710b223c2fab2d387c27e785e1ac
JBSL <bvkvgxvt@gmail.com>
2024-01-11 00:30:24 +0100

    should have done this 2 weeks ago :)

--- ProgramacionDeInteligencia/Raul/TaskListApi/app.py
+++ ProgramacionDeInteligencia/Raul/TaskListApi/app.py
@@ -1,9 +1,8 @@
-# save this as app.py
 import json
 import os.path
 import time
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from datetime import datetime
 
 import csv
@@ -11,13 +10,14 @@ import csv
 app = Flask(__name__)
 launch_time = time.time_ns()
 
+if not os.path.exists('./tasks.csv'):
+    open('./tasks.csv', 'w')
 
+
+# Listar todas las tareas
 @app.route('/tasks')
 def get_tasks():
     task_id = request.args.get("task", "")
-
-    if not os.path.exists('./tasks.csv'):
-        return ""
 
     file = open('./tasks.csv', newline='')
     csvreader = csv.reader(file)
@@ -33,10 +33,13 @@ def get_tasks():
     return json.dumps(rows)
 
 
+# Agregar una tarea
 @app.route('/tasks', methods=['POST'])
 def post_tasks():
     request_data = request.json
     task_description = request_data.get('Description', '')
+    if not task_description:
+        return jsonify({'msg': 'Task description is mandatory.'}), 400
 
     now = datetime.now()
     formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
@@ -59,35 +62,48 @@ def post_tasks():
     return json.dumps(task_to_record)
 
 
+# Editar una tarea en concreto TODO
 @app.route('/tasks', methods=['PUT'])
 def edit_task():
+    updated_flag = False
+    task_id = request.args.get("task", "")
+
     request_data = request.json
     task_description = request_data.get('Description', '')
+    if not task_description:
+        return jsonify({'msg': 'Task description is mandatory.'}), 400
 
     now = datetime.now()
     formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
 
-    max_id = 0
-
     file = open('tasks.csv', 'r+', newline='')
     csvreader = csv.reader(file)
 
+    task_to_update = ""
+    remaining_rows = []
     for row in csvreader:
-        if row and int(row[0]) > int(max_id):
-            max_id = row[0]
+        if row[0] == task_id:
+            row[1] = task_description
+            row[2] = 1
+            row[3] = formatted_now
+            task_to_update = row
+            updated_flag = True
+        remaining_rows.append(row)
 
-    task_to_record = [str(int(max_id) + 1), task_description, 0, formatted_now]
-    writer = csv.writer(file)
-    writer.writerows([task_to_record])
+    with open('tasks.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(remaining_rows)
 
-    file.close()
-
-    return json.dumps(task_to_record)
+    if updated_flag:
+        return json.dumps(task_to_update)
+    else:
+        return jsonify({'msg': 'Task id not found.'}), 400
 
 
 # Completar una tarea
 @app.route('/check-task', methods=['PUT'])
 def check_task():
+    updated_flag = False
     task_id = request.args.get("task", "")
 
     file = open('tasks.csv', newline='')
@@ -99,19 +115,24 @@ def check_task():
         if row[0] == task_id:
             row[2] = 1
             task_to_update = row
+            updated_flag = True
         remaining_rows.append(row)
 
-    # Write the remaining data back to the file
     with open('tasks.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(remaining_rows)
 
-    return json.dumps(task_to_update)
+    if updated_flag:
+        return json.dumps(task_to_update)
+    else:
+        return jsonify({'msg': 'Task id not found.'}), 400
 
 
 # Cambiar una tarea de completa a incompleta
 @app.route('/uncheck-task', methods=['PUT'])
 def uncheck_task():
+    updated_flag = False
+
     task_id = request.args.get("task", "")
 
     file = open('tasks.csv', newline='')
@@ -123,21 +144,25 @@ def uncheck_task():
         if row[0] == task_id:
             row[2] = 0
             task_to_update = row
+            updated_flag = True
         remaining_rows.append(row)
 
-    # Write the remaining data back to the file
     with open('tasks.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(remaining_rows)
 
-    return json.dumps(task_to_update)
+    if updated_flag:
+        return json.dumps(task_to_update)
+    else:
+        return jsonify({'msg': 'Task id not found.'}), 400
 
 
+# Borrar una tarea
 @app.route('/tasks', methods=['DELETE'])
 def delete_tasks():
+    deleted_flag = False
     row_to_delete = request.args.get("task", "")
 
-    # Read the data from the file and store all rows except the one to delete
     with open('tasks.csv', 'r', newline='') as file:
         rows = list(csv.reader(file))
         row_deleted = []
@@ -147,30 +172,31 @@ def delete_tasks():
                 remaining_rows.append(row)
             else:
                 row_deleted = row
+                deleted_flag = True
 
-    # Write the remaining data back to the file
     with open('tasks.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(remaining_rows)
 
-    return json.dumps(row_deleted)
+    if deleted_flag:
+        return json.dumps(row_deleted)
+    else:
+        return jsonify({'msg': 'Task id not found.'}), 400
 
 
 # Borrar todas las tareas finalizadas
 @app.route('/completed-tasks', methods=['DELETE'])
 def delete_completed_tasks():
-    # Read the data from the file and store all rows except the one to delete
     with open('tasks.csv', 'r', newline='') as file:
         rows = list(csv.reader(file))
         rows_deleted = []
         remaining_rows = []
         for i, row in enumerate(rows):
-            if row[2] == "1":
+            if row[2] == "0":
                 remaining_rows.append(row)
             else:
                 rows_deleted.append(row)
 
-    # Write the remaining data back to the file
     with open('tasks.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(remaining_rows)
--- ProgramacionDeInteligencia/Raul/TaskListApi/test.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-sys.executable = sys.executable.replace('\\App', '\\..\\..\\App')
